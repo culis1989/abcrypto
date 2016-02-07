27a94c4699b4ec3bde532aad4731311bd0cf6847
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-02-07 21:53:15 +0100

    category wip2

--- category.php
+++ category.php
@@ -10,15 +10,19 @@
         $excerpt = get_the_excerpt();
         $link = get_permalink();
         $tags = get_the_tags();
+        $date = get_the_date();
 
+        echo '<div class="date">'.$date.'</div>';
         echo '<h3><a href="'.$link.'">'.$title.'</a></h3>';
         if($tags){
+          echo '<div class="tags">';
           foreach ($tags as $tag) {
             echo '<a href="/tag/'.$tag->slug.'">'.$tag->name.'</a> / ';
           }
+          echo '</div>';
         }
         echo '<p>'.$excerpt.'</p>';
-        echo '<a class="button" href="'.$link.'">READ MORE</a>';
+        echo '<a class="button orange" href="'.$link.'">READ MORE</a>';
       echo '</li>';
   	}
     echo '</ul>';
