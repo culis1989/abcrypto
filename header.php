7ed925d8470f0cc469da49b0ed25dd361a8546a9
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-03-11 04:34:57 +0100

    changes from livewebsite

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="author" content="SitePoint">
+  <meta name="author" content="Goran Kunjadić">
   <!--[if lt IE 9]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
